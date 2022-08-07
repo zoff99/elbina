b3c16fe37d5071e52a59df550130bdac008b1b5c
zoff99 <zoff99@users.noreply.github.com>
2022-08-07 17:09:22 +0200

    MacOS CI

--- .ci/ci_macos.sh
+++ .ci/ci_macos.sh
@@ -4,10 +4,7 @@ cd elbina/
 
 echo 'mb_groupadd gg 776
 mb_useradd xxx 333 /home/xxx2 "reowprü3+3+ür23+" gg
-mkdir -p /Users/xxx2
-chown xxx:gg /Users/xxx2
 mb_install_essentials
-mb_installrepopkg qtox
 ' > test.txt
 
 ./elbina.sh test.txt
